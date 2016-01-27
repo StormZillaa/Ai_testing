28e30280db43a8ac30a951eece6590501d23eafc
StormZillaa <stormzilla9933@gmail.com>
2016-01-27 12:15:53 -0600

    Update run.rs

--- run.rs
+++ run.rs
@@ -1,15 +1,18 @@
+mod logic.rs;
 fn run(x:i32) -> bool{
-  if(x/5 == int(x/5)){
-  x = x/5;
+  let add:bool = subtr:bool = mult:bool = divd:bool = true;
+  if(x/2 == int(x/2) && divd == true){
+  x = x/2;
+  divd = logic(x, divd, )
   }
-  if(x > 10){
+  if(x > 50 && subtr == true){
   x = x - 1;
   }
-  if(x < 10){
-  x + 1;
+  if(x < 50 && add == true){
+  x = x + 1;
   }
-  if(x*2 == 10){
-  
+  if(x*2 > 10 && mult == true){
+  x = x*2;
   }
   if(x == 50){
   return false;
