bdf8c3528e9f297a68c2a29e362aa5c600e03aba
StormZillaa <stormzilla9933@gmail.com>
2016-01-27 11:53:53 -0600

    Update and rename rand.rs to run.rs

---
+++ run.rs
@@ -0,0 +1,17 @@
+fn run(x:i32) -> bool{
+  if(x/5 == int(x/5)){
+  x = x/5;
+  }
+  if(x > 10){
+  x = x - 1;
+  }
+  if(x < 10){
+  x + 1;
+  }
+  if(x*2 == 10){
+  
+  }
+  if(x == 50){
+  return false;
+  }
+}
