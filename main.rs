0f09d82c21c78a7965a105a8102c5f889a7776b6
StormZillaa <stormzilla9933@gmail.com>
2016-01-27 12:19:09 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,7 +1,5 @@
 mod 
 fn main() -> int{
-  let add:bool = subtr:bool = mult:bool = divd:bool = true;
-  let adt:i32 = subt:i32 = miltt:i32 = divt:i32 = 0;
   let x:i32 = 25;
   let active = true;
   while (active == true){
