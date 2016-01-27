739abc2480259692554b37ed3c29632cafc4988d
StormZillaa <stormzilla9933@gmail.com>
2016-01-27 12:18:32 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,10 +1,12 @@
 mod 
 fn main() -> int{
+  let add:bool = subtr:bool = mult:bool = divd:bool = true;
+  let adt:i32 = subt:i32 = miltt:i32 = divt:i32 = 0;
   let x:i32 = 25;
   let active = true;
   while (active == true){
       let x = run(x);
-      if(x == true){
+      if(x == false){
         active = false;
         }
         else{
