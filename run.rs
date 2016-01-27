8d04c258780f9600c72a1024d4a7e3169c941eae
StormZillaa <stormzilla9933@gmail.com>
2016-01-27 12:17:51 -0600

    Update run.rs

--- run.rs
+++ run.rs
@@ -1,9 +1,10 @@
 mod logic.rs;
 fn run(x:i32) -> bool{
   let add:bool = subtr:bool = mult:bool = divd:bool = true;
+  let adt:i32 = subt:i32 = miltt:i32 = divt:i32 = 0;
   if(x/2 == int(x/2) && divd == true){
   x = x/2;
-  divd = logic(x, divd, )
+  divd = logic(x, divt)
   }
   if(x > 50 && subtr == true){
   x = x - 1;
