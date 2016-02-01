7cf4250d6406532b3afcdc4727a6278f3fab430b
StormZillaa <stormzilla9933@gmail.com>
2016-02-01 09:30:02 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,9 +1,45 @@
+fn logic(x:i32,times:i32,name:bool) -> bool{
+  if x<50 && times >= 3{
+  bool = false;
+  }
+  if x>50 {
+  bool = true;
+  times = times + 1;
+  logic(x,times,name);
+  }
+fn run(x:i32) -> bool{
+  let on:bool = true;
+    let add:bool = subtr:bool = mult:bool = divd:bool = true;
+    let adt:i32 = subt:i32 = multt:i32 = divt:i32 = 0;
+  while on == true {
+      if x/2 > 1 && divd == true{
+        x = x/2;
+        divd = logic(x, divt, divd);
+      }
+      if x > 50 && subtr == true {
+        x = x - 1;
+        subtr = logci(x, subt, subtr);
+      }
+      if x < 50 && add == true {
+        x = x + 1;
+        add = logic(x, adt, add);
+      }
+      if x*2 > 10 && mult == true {
+        x = x*2;
+        mult = logic(x, multt, mult);
+      }
+      if x == 50 {
+        return false;
+      }
+     println!("{}",x);
+    }
+  }}
 fn main() -> int{
   let x:i32 = 25;
   let active = true;
-  while (active == true){
+  while active == true {
       let x = run(x);
-      if(x == false){
+      if x == false {
         active = false;
         }
         else{
@@ -13,30 +49,3 @@ fn main() -> int{
   println!("we have ended");
   return 0;
 }
-fn run(x:i32) -> bool{
-  let on:bool = true;
-    let add:bool = subtr:bool = mult:bool = divd:bool = true;
-    let adt:i32 = subt:i32 = multt:i32 = divt:i32 = 0;
-  while(on == true){
-      if(x/2 == int(x/2) && divd == true){
-        x = x/2;
-        divd = logic(x, divt, divd);
-      }
-      if(x > 50 && subtr == true){
-        x = x - 1;
-        subtr = logci(x, subt, subtr);
-      }
-      if(x < 50 && add == true){
-        x = x + 1;
-        add = logic(x, adt, add);
-      }
-      if(x*2 > 10 && mult == true){
-        x = x*2;
-        mult = logic(x, multt, mult);
-      }
-      if(x == 50){
-        return false;
-      }
-     println!("{}",x);
-    }
-  }
